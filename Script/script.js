c43214cda748dfb9413e816a999626a5c909b426
MaxiMeriggi <72845427+MaxiMeriggi@users.noreply.github.com>
2024-07-31 01:01:32 -0300

    Added duplicate function

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,4 @@
-let inputArray = document.querySelectorAll('.input');
+var currentGlobalId = 0;
 
 GetElementById = (id) => {
     return document.getElementById(id)
@@ -6,23 +6,45 @@ GetElementById = (id) => {
 
 FilterNaN = (int) => {
     int = parseFloat(int)
-
+    
     if(isNaN(int)){
         return 0;
     }
-
+    
     return int;
 }
 
-CalcIp = () => {
-    let Productivity = FilterNaN(GetElementById("Productivity").value);
-    let Factories = FilterNaN(GetElementById("Factories").value);
-    let Crafting = FilterNaN(GetElementById("Crafting").value);
+
+SetEventListener = () => {
+    document.querySelectorAll('.input').forEach(function (e) {
+        e.addEventListener('input', function(e) {
+            let currentId = e.target.id.split('_')[1]
+            CalcIps(currentId)
+        })
+    })
+
+    document.querySelectorAll('#Duplicate').forEach(function(e){
+        e.addEventListener('click', DuplicateCalc)
+    }) 
     
-    let Recipe = FilterNaN(GetElementById("Recipe").value);
+}
+
+
+CalcIps = (e) => {
     
-    let ResultSec = GetElementById("ResultSec");
-    let ResultMin = GetElementById("ResultMin");
+    let Productivity = FilterNaN(GetElementById("Productivity_" + e).value);
+    //Since Prod min value should always be 1, i check for the value before advancing
+    if(Productivity <= 1){
+        Productivity = 1
+    }
+
+    let Factories = FilterNaN(GetElementById("Factories_" + e).value);
+    let Crafting = FilterNaN(GetElementById("Crafting_" + e).value);
+    
+    let Recipe = FilterNaN(GetElementById("Recipe_" + e).value);
+    
+    let ResultSec = GetElementById("ResultSec_" + e);
+    let ResultMin = GetElementById("ResultMin_" + e);
     
     let calcSec = FilterNaN((Productivity * Factories * Crafting) / Recipe );
     
@@ -31,7 +53,20 @@ CalcIp = () => {
     
 }
 
+DuplicateCalc = () => {
+    let element = document.getElementById('Calculate_0');
+    let clone = element.cloneNode(true);
+    currentGlobalId++;
 
-inputArray.forEach(function (e) {
-    e.addEventListener('input', CalcIp)
-})
+    clone.querySelectorAll("input").forEach(function(e){
+        let oldId = e.id.split('_');
+        e.id = oldId[0] + "_" + (currentGlobalId)
+        e.value = null
+    })
+
+    document.body.append(clone);
+    SetEventListener()
+    
+}
+
+SetEventListener()
